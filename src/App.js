dc825e9a60e7ae6181ab785dfe7f826c159ecaff
liutie <liutie@cashim.com>
2024-12-12 13:47:02 +0800

    复现

--- src/App.js
+++ src/App.js
@@ -1,24 +1,55 @@
-import logo from './logo.svg';
+import {createRoot} from 'react-dom/client'
 import './App.css';
+import {Modal, ConfigProvider} from "antd";
+import {useEffect} from "react";
+
+
+const ThemeProvider = (props) => {
+  return <ConfigProvider theme={{
+    "cssVar": true,
+    "hashed": false,
+  }}>
+    {props.children}
+  </ConfigProvider>
+}
+
+
+const T = (props) => {
+  const [modal, context] = Modal.useModal()
+  useEffect(() => {
+    modal.confirm({title: '123', content: 123, onCancel: props.onCancel});
+  }, [])
+  return <ThemeProvider>{context}</ThemeProvider>
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider>
+      <div className="App">
+        <div>
+          <button onClick={() => {
+            Modal.info({title: "hello", maskClosable: true})
+          }}>Modal.info
+          </button>
+        </div>
+        <div>
+          <button onClick={() => {
+            const ModalContainer = document.createElement('div')
+            ModalContainer.className = 'GKModalShowClass'
+            document.body.appendChild(ModalContainer)
+            const reactRoot = createRoot(ModalContainer)
+            reactRoot.render(
+              <T onCancel={() => {
+                reactRoot.unmount()
+                // 销毁容器
+                ModalContainer.remove()
+              }}/>,
+            )
+          }}>hook
+          </button>
+        </div>
+      </div>
+    </ThemeProvider>
   );
 }
 
